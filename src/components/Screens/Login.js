1dc687c96ef9688117decfe716d834b5b2b1753c
gabriel.pedrosaf <gabriel.pedrosaf@gmail.com>
2022-06-20 15:28:27 -0300

    Login sendo efetuado

--- src/components/Screens/Login.js
+++ src/components/Screens/Login.js
@@ -1,7 +1,49 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Button, TextInput, Text, ImageBackground } from 'react-native'
+import React, { Component, useEffect, useState } from 'react';
+import { StyleSheet, View, Button, TextInput, Text, ImageBackground, TouchableOpacity } from 'react-native'
+import api from '../../services/api';
+
 
 const Login = ({ navigation }) => {
+  	const [user, setUser] = useState();
+  	const [password, setPassword] = useState();
+
+
+	const logar = () => {
+		console.log("logar");
+		api
+		.post("/user/login",{
+			email: "rsouza@rsouza.com",
+			password: "123"
+		})
+		.then((response) => {
+			if(response.data.success) {
+				console.log(response.data.message)
+        		navigation.navigate('Profile', { name: 'Jane' })
+
+			}
+		})
+		.catch((err) => {
+			console.error("ops! ocorreu um erro" + err);
+		});
+	}
+
+	// useEffect(() => {
+	// 	api
+	// 	.post("/user/login",{
+	// 		email: "rsouza@rsouza.com",
+	// 		password: "123"
+	// 	})
+	// 	.then((response) => {
+	// 		if(response.data.success) {
+	// 			console.log(response.data.message)
+	// 		}
+	// 	})
+	// 	.catch((err) => {
+	// 		console.error("ops! ocorreu um erro" + err);
+	// 	});
+	// }, []);
+  
+
   return (
     <View style={style.loginContainer}>
       <ImageBackground source={require('../../img/login.png')} resizeMode="cover" style={style.login_img}>
@@ -24,6 +66,14 @@ const Login = ({ navigation }) => {
               />
               <Text>Icone</Text>
             </View>
+          </View>
+          <View>
+            <TouchableOpacity style={style.buttonLogin} onPress={ () => logar()}>
+              <Text style={style.textButtonLogin}>Logar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.buttonRegister}>
+              <Text style={style.textButtonRegister}>Registrar</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ImageBackground>
@@ -52,16 +102,18 @@ const style = StyleSheet.create({
   login_img:{
     flex: 1,
     justifyContent: 'center',
-    opacity: .8
+    opacity: .7
   },  
   inputFieldName:{
     borderWidth: 1,
     borderColor: '#000',
     height: 40,
+    paddingLeft: 10
   },
   inputFieldPassword:{
     height: 40,
-    flex: 1
+    flex: 1,
+    paddingLeft: 10
   },
   label:{
     fontWeight: '600',
@@ -74,6 +126,32 @@ const style = StyleSheet.create({
     alignItems: 'center',
     borderWidth: 1,
     borderColor: '#000',
+  },
+  buttonLogin: {
+    margin: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "green"
+  },
+  textButtonLogin: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+  buttonRegister: {
+    margin: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "blue"
+  },
+  textButtonRegister: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
   }
 })
 
